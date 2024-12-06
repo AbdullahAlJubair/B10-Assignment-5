4af627732c5ca1b1594bc3ddd0cc5031b037d52f
AbdullahAlJubair <fabdullahjubair@gmail.com>
2024-12-07 01:43:16 +0600

    Donation tab functionality added by js

--- js/script.js
+++ js/script.js
@@ -16,5 +16,37 @@ function historyTab() {
 }
 
 
+let cards = document.querySelectorAll('.donateBtn');
+for (const card of cards) {
+    card.addEventListener('click', function () {
+        // Card Old Amount
+        const oldDonationAmount = this.parentElement.children[0].children[1].children[0];
+        const oldDonation = Number(oldDonationAmount.innerText);
 
+        // Card Input Field Amount
+        const inputDonationAmount = this.parentElement.children[3].children[0];
+        const inputDonation = parseFloat(inputDonationAmount.value);
+
+        // Header Main Balance Amount
+        const mainBalanceAmount = document.getElementById('mainBalance');
+        const mainBalance = Number(mainBalanceAmount.innerText);
+
+        if (isNaN(inputDonation) || inputDonation < 0) {
+            return alert('Please Input Valid Number'), inputDonationAmount.value = '';
+        }
+        else if (inputDonation > mainBalance) {
+            return alert("You don't have enough balance "), inputDonationAmount.value = '';
+        }
+
+        // New Donation Amount Assigned
+        const newDonation = oldDonation + inputDonation;
+        oldDonationAmount.innerText = newDonation;
+        // New Main Balance AMount Assigned
+        const newMainBalance = mainBalance - inputDonation;
+        mainBalanceAmount.innerText = newMainBalance;
+
+        inputDonationAmount.value = '';
+        document.getElementById('my_modal_1').showModal()
+    })
+}
 
